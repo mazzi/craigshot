cfb29feaf98469261c17e862a67cd750c2524b16
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2013-08-22 12:40:45 +0200

    Better modularization.

--- post.py
+++ post.py
@@ -8,46 +8,63 @@ import os
 import sys
 import configparser
 
-# Keys in http://www.tumblr.com/oauth/apps
-config = configparser.Configparser()
-config.read('config.ini')
 
-print "Taking screenshot ..."
-display = Display(visible=0, size=(config['SCREEN']['WIDTH'], config['SCREEN']['HEIGHT']))
-display.start()
+def screenshot( url, config ):
+    display = Display( visible=0, size=( config['SCREEN']['WIDTH'], config['SCREEN']['HEIGHT'] ) )
+    display.start()
 
-browser = webdriver.Firefox()
-browser.get(sys.argv[1])
-browser.save_screenshot('screenie.png')
-browser.quit()
+    browser = webdriver.Firefox()
+    browser.get( url )
+    browser.save_screenshot( 'screenie.png' )
+    browser.quit()
 
-display.stop()
+    display.stop()
 
-print "Done!"
-print "Now posting to %s ..." % config['DEFAULT']['BLOG']
+def post ( config ):
 
-api = TumblrAPIv2(  config['DEFAULT']['CONSUMER_KEY'], \
-                    config['DEFAULT']['CONSUMER_SECRET'], \
-                    config['DEFAULT']['OAUTH_TOKEN'], \
-                    config['DEFAULT']['OAUTH_TOKEN_SECRET'] )
+    api = TumblrAPIv2(  config['DEFAULT']['CONSUMER_KEY'], \
+                        config['DEFAULT']['CONSUMER_SECRET'], \
+                        config['DEFAULT']['OAUTH_TOKEN'], \
+                        config['DEFAULT']['OAUTH_TOKEN_SECRET'] )
 
-date  = time.gmtime(os.path.getmtime('./screenie.png'))
-post = {
-    'type' : 'photo',
-    'date' : time.strftime ("%Y-%m-%d %H:%M:%S", date),
-    'data' : './screenie.png',
-    'tags' : time.strftime ("%Y", date) + ", photo",
-    'caption' : time.strftime ("%B %d / %Y", date)
-}
+    date  = time.gmtime(os.path.getmtime('./screenie.png'))
+    post = {
+        'type' : 'photo',
+        'date' : time.strftime ("%Y-%m-%d %H:%M:%S", date),
+        'data' : './screenie.png',
+        'tags' : time.strftime ("%Y", date) + ", photo",
+        'caption' : time.strftime ("%B %d / %Y", date)
+    }
 
-try:
-    response = api.createPhotoPost( config['DEFAULT']['BLOG'], post)
-    if 'id' in response:
-        print response['id']
+    try:
+        print post
+        print config['DEFAULT']['BLOG']
+        response = api.createPhotoPost( config['DEFAULT']['BLOG'], post)
+        if 'id' in response:
+            print response['id']
+        else:
+            print response
+    except APIError:
+        print "Error Uploading file!"
+    finally:
         os.remove('./screenie.png')
-    else:
-        print response
-except APIError:
-    print "Error Uploading file!"
 
-print "Done! "
+def main( url ):
+    # Keys in http://www.tumblr.com/oauth/apps
+    config = configparser.ConfigParser()
+    config.read( 'config.ini' )
+
+    print "Taking screenshot ..."
+
+    screenshot ( url, config  )
+    
+    print "Done!"
+    print "Now posting to %s ..." % config['DEFAULT']['BLOG']
+
+    post ( config )
+
+    print "Done! "
+
+
+if __name__ == "__main__":
+    main( sys.argv[1] )
